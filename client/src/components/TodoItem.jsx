4d6db2896d74207ae1294b53789fd95e6b34641d
natalia-se <flaunder1000@gmail.com>
2022-04-09 11:22:18 +0200

    add TodoItem component

---
+++ client/src/components/TodoItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TodoItem = () => {
+  return (
+    <div className="Card">
+      {/* <div className="Card--text">
+        <h1 className={checkTodo}>{todo.name}</h1>
+        <span className={checkTodo}>{todo.description}</span>
+      </div>
+      <div className="Card--button">
+        <button
+          // onClick={() => updateTodo(todo)}
+          className={todo.status ? `hide-button` : "Card--button__done"}
+        >
+          Complete
+        </button>
+        <button
+          // onClick={() => deleteTodo(todo._id)}
+          className="Card--button__delete"
+        >
+          Delete
+        </button>
+      </div> */}
+    </div>
+  );
+};
+
+export default TodoItem;
