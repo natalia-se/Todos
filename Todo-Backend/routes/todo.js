6e2320243b1e01cd05fdc9f2905833b758b407cc
natalia-se <flaunder1000@gmail.com>
2022-04-09 09:39:51 +0200

    Added api to make the todo completed

--- Todo-Backend/routes/todo.js
+++ Todo-Backend/routes/todo.js
@@ -30,4 +30,43 @@ router.get("/", requireLogin, async (req, res) => {
   }
 });
 
+// Mark todo as completed
+// app.patch("/api/todos/:id", (req, res) => {
+//   const todo = todos.find((todo) => todo.id == req.params.id);
+//   if (!todo) return res.sendStatus(404);
+//   todo.completed = !todo.completed;
+//   res.json(todo);
+// });
+router.patch("/:id", requireLogin, async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.user;
+
+  try {
+    const todo = await Todo.findById(id);
+    todo.isDone = !todo.isDone;
+
+    if (todo.userId.toString() === userId) {
+      try {
+        const updatedTodo = await todo.save();
+
+        //const updatedMessage = await Todo.findByIdAndUpdate(id, update)
+        // const updatedMessage = await Todo.findByIdAndUpdate(
+        //   id,
+        //   {
+        //     $set: req.body,
+        //   },
+        //   { new: true }
+        // );
+        res.status(200).json(updatedTodo);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json("You can update only your message");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
