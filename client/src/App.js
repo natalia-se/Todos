7f50caf78f75798878e2f9ef8494ec4d22191af6
natalia-se <flaunder1000@gmail.com>
2022-04-09 11:20:54 +0200

    add routes

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
